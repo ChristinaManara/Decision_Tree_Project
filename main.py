69ac08fe46d193c4335436ab919430354faf35ee
christinamanara <manara1024@gmail.com>
2021-06-08 17:38:15 +0300

    changes

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    # Use pandas to read loan_data.csv as a dataframe called loans.
+    loans = pd.read_csv('loan_data.csv')
+    print(loans)
+    # Check out the info(), head(), and describe() methods on loans.
+    print(loans.info())
+    print(loans.head())
+    print(loans.describe())
+    # Create a histogram of two FICO distributions on top of each other, one for each credit.policy outcome.
+    sns.heatmap(loans.isnull(), yticklabels=False, cbar=False, cmap='viridis')
+    x = loans[loans['credit.policy'] == 1]['fico']
+    y = loans[loans['credit.policy'] == 0]['fico']
+    plt.figure(figsize=(10, 6))
+    x.hist(alpha=0.5, color='blue', bins=30, label='Credit.Policy=1')
+    y.hist(alpha=0.5, color='red', bins=30, label='Credit.Policy=0')
+    plt.legend()
+    plt.xlabel('FICO')
+    plt.show()
+    #  Create a similar figure, except this time select by the not.fully.paid column
+    x = loans[loans['not.fully.paid'] == 1]['fico']
+    y = loans[loans['not.fully.paid'] == 0]['fico']
+    plt.figure(figsize=(10, 6))
+    x.hist(alpha=0.5, color='blue', bins=30, label='not.fully.paid=1')
+    y.hist(alpha=0.5, color='red', bins=30, label='not.fully.paid=0')
+    plt.legend()
+    plt.xlabel('FICO')
+    plt.show()
+
+
+
 
 
 # Press the green button in the gutter to run the script.
